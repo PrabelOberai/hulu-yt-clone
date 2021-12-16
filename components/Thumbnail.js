9a4d54fb32640857405df3c415ebc33270dda515
Prabel <prabel.oberai@gmail.com>
2021-12-16 11:10:45 +0530

    Vercel Issue - Image alt attribute added

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -7,7 +7,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
     return (
         <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image 
-                layout="responsive" src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920}
+                layout="responsive" alt="" src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920}
             />
             <div className="p-2">
                 <p className="truncate max-w-md">{result.overview}</p>
